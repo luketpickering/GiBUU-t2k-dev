37fb4a3f24fca6a47ddf66c28cb2757e9e3e39b5
Luke Pickering <luketpickering@googlemail.com>
2016-07-18 12:11:21 +0100

    Fixes fix to lepton2p2h patch.

--- setup.sh
+++ setup.sh
@@ -22,7 +22,6 @@ fi
 if [[ -e "${SETUPDIR}/GiBUUInstall/release2016/bin/gibuu" ]]; then
   if ! [[ ":$PATH:" == *":${SETUPDIR}/GiBUUInstall/release2016/bin:"* ]]; then
     export GIBUU=${SETUPDIR}/GiBUUInstall/release2016
-    export GIBUUINPUTS=${SETUPDIR}/GiBUUInstall/buuinput_2016
     export PATH=${GIBUU}/bin:$PATH
   fi
 fi
